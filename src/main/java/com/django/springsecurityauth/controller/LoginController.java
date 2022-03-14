794c820d961c0d6aaa5f0d70abc5410ed95d88ed
django123 <edougajean@gmail.com>
2022-03-14 13:02:25 +0100

    first commit

---
+++ src/main/java/com/django/springsecurityauth/controller/LoginController.java
@@ -0,0 +1,83 @@
+package com.django.springsecurityauth.controller;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+public class LoginController {
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    public LoginController(OAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
+    }
+
+    @RolesAllowed("USER")
+    @RequestMapping("/**")
+    public String getUser(){
+        return  "Welcome User";
+    }
+
+    @RolesAllowed("ADMIN")
+    @RequestMapping("/admin")
+    public String getAdmin(){
+        return "Welcome Admin";
+    }
+
+    @RequestMapping("/*")
+    public String getUserInfo(Principal user) {
+        StringBuffer userInfo= new StringBuffer();
+        if(user instanceof UsernamePasswordAuthenticationToken){
+            userInfo.append(getUserNamePasswordLoginInfo(user));
+        }
+        else if(user instanceof OAuth2AuthenticationToken){
+            userInfo.append(getOauth2LoginInfo(user));
+        }
+        return userInfo.toString();
+    }
+
+
+    private StringBuffer getUserNamePasswordLoginInfo(Principal user){
+        StringBuffer userName = new StringBuffer();
+        UsernamePasswordAuthenticationToken token = ((UsernamePasswordAuthenticationToken)user);
+        if (token.isAuthenticated()){
+            User u = (User) token.getPrincipal();
+            userName.append("Welcome," + u.getUsername());
+        }else {
+            userName.append("NA");
+        }
+        return userName;
+    }
+
+
+    private StringBuffer getOauth2LoginInfo(Principal user){
+
+        StringBuffer protectedInfo = new StringBuffer();
+
+        OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
+        OAuth2AuthorizedClient authClient = this.authorizedClientService.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
+        if(authToken.isAuthenticated()){
+
+            Map<String,Object> userAttributes = ((DefaultOAuth2User) authToken.getPrincipal()).getAttributes();
+
+            String userToken = authClient.getAccessToken().getTokenValue();
+            protectedInfo.append("Welcome, " + userAttributes.get("name")+"<br><br>");
+            protectedInfo.append("e-mail: " + userAttributes.get("email")+"<br><br>");
+            protectedInfo.append("Access Token: " + userToken+"<br><br>");
+        }
+        else{
+            protectedInfo.append("NA");
+        }
+        return protectedInfo;
+    }
+}
---
+++ src/test/java/com/django/springsecurityauth/SpringsecurityauthApplicationTests.java
@@ -0,0 +1,25 @@
+package com.django.springsecurityauth;
+
+import com.django.springsecurityauth.controller.LoginController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class SpringsecurityauthApplicationTests {
+
+    @Autowired
+    private LoginController loginController;
+
+    @Test
+    void contextLoads() {
+
+        assertThat(loginController).isNotNull();
+    }
+
+}
